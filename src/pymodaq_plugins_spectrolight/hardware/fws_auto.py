039fa787cdd1e8b3962a0402c3df4caae29ac376
Sébastien Weber <seba.weber@gmail.com>
2023-02-13 16:51:22 +0100

    added minimal working example and using clear message functionnality from dll

---
+++ src/pymodaq_plugins_spectrolight/hardware/fws_auto.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+"""
+Created the 30/01/2023
+
+@author: Sebastien Weber
+
+Wrapper around the dll library: FWS Auto DLL
+From documentation:
+https://www.spectrolightinc.com/board/detail/1/?board_id=140
+
+"""
+
+import os
+import sys
+from pathlib import Path
+from typing import List, Union, Tuple
+from time import sleep
+import clr
+from System import String, Double
+
+from pymodaq.daq_utils import daq_utils as utils
+from pymodaq.daq_utils.messenger import messagebox
+from pymodaq.daq_utils.enums import BaseEnum
+
+logger = utils.set_logger(utils.get_module_name(__file__))
+
+
+if utils.is_64bits():
+    path_dll = str(Path(r'C:\FWSPoly'))
+else:
+    messagebox(severity='critical', title='FWS-Auto Dll', text='The dll is only available for 64bits systems')
+
+sys.path.append(path_dll)
+polydll = clr.AddReference('PolyDLL')
+
+from ISM_Device import ClassPoly  # ISM_Device is the assembly name within PolyDll, found this using dotPeek
+# application over the dll. ClasPoly is the .net object to use for communication and described in the documentation
+
+
+class PolyError(Exception):
+    pass
+
+
+class FWSAuto:
+    def __init__(self):
+        self._net_wrapper = ClassPoly()
+
+    def connect(self, calibration_path: Union[str, Path]) -> str:
+        ret = self._net_wrapper.PolyConnect(String(str(calibration_path)))
+        return self._net_wrapper.GetStringMsg(ret)
+
+    def disconnect(self) -> str:
+        ret = self._net_wrapper.Disconnect()
+        return self._net_wrapper.GetStringMsg(ret)
+
+    def get_device_status(self) -> str:
+        ret = self._net_wrapper.GetDeviceStatus()
+        return self._net_wrapper.GetStringMsg(ret)
+
+    def is_device_enabled(self) -> bool:
+        ret = self._net_wrapper.GetDeviceEnabled()
+        return bool(ret)
+
+    def get_com_port(self) -> str:
+        ret = self._net_wrapper.GetComPortNumber()
+        return str(ret)
+
+    def get_device_info(self) -> Tuple[str]:
+        ret, model, serial, range = self._net_wrapper.GetInforData(String(''), String(''), String(''))
+        if ret != 0:
+            raise PolyError(self._net_wrapper.GetStringMsg(ret))
+        return model, serial, range
+
+    @property
+    def wavelength(self) -> Tuple[float]:
+        ret, sw, cw, lw, fwhm = self._net_wrapper.GetCurrentWavelength('', '', '', '')
+        if ret != 0:
+            raise PolyError(self._net_wrapper.GetStringMsg(ret))
+        return cw, fwhm
+
+    @wavelength.setter
+    def wavelength(self, cw_fwhm: Tuple[float]):
+        ret = self._net_wrapper.SetWavelength(cw_fwhm[0], cw_fwhm[1])
+        print(f'SetWavelength {self._net_wrapper.GetStringMsg(ret)}')
+
+    def no_filtering(self) -> str:
+        ret = self._net_wrapper.GoBlankPosition()
+        return self._net_wrapper.GetStringMsg(ret)
+
+    def reset(self) -> str:
+        ret = self._net_wrapper.DeviceReset()
+        return self._net_wrapper.GetStringMsg(ret)
+
+
+if __name__ == '__main__':
+    fws = FWSAuto()
+    msg = fws.connect(r'C:\FWSPoly\20220818_FAPVIS00222.ism')
+    print(msg)
+    try:
+        print(f'COM: {fws.get_com_port()}')
+        print(f'Infos: {fws.get_device_info()}')
+
+        msg = fws.get_device_status()
+        print(msg)
+
+        fws.wavelength = (530, 5)
+        msg = fws.get_device_status()
+        print(msg)
+        print(f'Center wl, FWHM: {fws.wavelength}')
+
+    except PolyError as pe:
+        print(str(pe))
+    finally:
+        fws.disconnect()
+
+    
---
+++ src/pymodaq_plugins_spectrolight/hardware/mwe.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created the 30/01/2023
+
+@author: Sebastien Weber
+
+Wrapper around the dll library: FWS Auto DLL
+From documentation:
+https://www.spectrolightinc.com/board/detail/1/?board_id=140
+
+"""
+
+import os
+import sys
+from pathlib import Path
+from typing import List, Union, Tuple
+from time import sleep
+import clr
+from System import String, Double
+
+
+
+
+path_dll = str(Path(r'C:\FWSPoly'))
+sys.path.append(path_dll)
+polydll = clr.AddReference('PolyDLL')
+
+from ISM_Device import ClassPoly
+
+
+if __name__ == '__main__':
+    poly = ClassPoly()
+    calibration_path = r'C:\FWSPoly\20220818_FAPVIS00222.ism'
+    print(f'Connecting: {poly.GetStringMsg(poly.PolyConnect(calibration_path))}')
+    try:
+        print(f'Status: {poly.GetStringMsg(poly.GetDeviceStatus())}')
+
+        print(f'Enable: {poly.GetDeviceEnabled()}')
+
+        print(f'Setting Wavelength: {poly.GetStringMsg(poly.SetWavelength(532., 10.))}')
+        sw, cw, lw, fwhm = '', '', '', ''
+        ret, sw, cw, lw, fwhm = poly.GetCurrentWavelength(sw, cw, lw, fwhm)
+        print(f'Get Wavelength: {poly.GetStringMsg(ret)}, {cw}, {fwhm}')
+
+
+    except Exception as e:
+        print(e)
+    finally:
+        print(f'Disconnecting: {poly.GetStringMsg(poly.Disconnect())}')
